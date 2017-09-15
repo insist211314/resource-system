1028b48806fb52d72757700158a8704a7ef9682f
李尚豇 <lishangjiang@9zhitx.com>
2017-09-15 17:35:10 +0800

    添加sql

---
+++ sql/t_business_app.sql
@@ -0,0 +1,3 @@
+INSERT INTO `t_business_app` VALUES (50, 'biz-social-task', '192.168.1.213', 2154, 0, '2017-5-12 16:48:10', '2017-5-12 16:48:10');
+INSERT INTO `t_business_app` VALUES (55, 'biz-social-task', '192.168.1.118', 2153, 0, '2017-5-12 16:51:42', '2017-5-12 16:51:42');
+INSERT INTO `t_business_app` VALUES (60, 'biz-social-task', '192.168.2.51', 2153, 0, '2017-5-17 12:27:54', '2017-5-17 12:27:54');
